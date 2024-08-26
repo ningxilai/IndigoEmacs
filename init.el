c988640877b59faaa90bb423d571de9749fb1409
Iris Lennon <64725627+ningxilai@users.noreply.github.com>
2024-08-26 12:05:07 +0800

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,13 @@
+;; Package Manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (load bootstrap-file nil 'nomessage))
+
 ;; Make emacs startup faster
 (setq gc-cons-threshold (* 100 1024 1024))
  
