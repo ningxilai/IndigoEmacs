cce51932e03b178079e796f6df0ee723513ec4c9
Iris Lennon <64725627+ningxilai@users.noreply.github.com>
2024-08-26 23:55:24 +0800

    Update early-init.el

--- early-init.el
+++ early-init.el
@@ -1,41 +1,33 @@
-;;;by CentaurEmacs/early-init.el
+;;;by CentaurEmacs/early-init.el && ItyIty/VanillaDoom/early-init.el
 
-;; Defer garbage collection further back in the startup process
-(setq gc-cons-threshold most-positive-fixnum)
-
-;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
-;; packages are compiled ahead-of-time when they are installed and site files
-;; are compiled when gccemacs is installed.
 (setq native-comp-deferred-compilation nil ;; obsolete since 29.1
       native-comp-jit-compilation nil)
 
-;; Package initialize occurs automatically, before `user-init-file' is
-;; loaded, but after `early-init-file'. We handle package
-;; initialization, so we must prevent Emacs from doing it early!
-(setq package-enable-at-startup nil)
+(setq package-enable-at-startup t)
 
-;; `use-package' is builtin since 29.
-;; It must be set before loading `use-package'.
 (setq use-package-enable-imenu-support t)
 
-;; In noninteractive sessions, prioritize non-byte-compiled source files to
-;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
-;; to skip the mtime checks on every *.elc file.
-(setq load-prefer-newer noninteractive)
-
-;; Explicitly set the prefered coding systems to avoid annoying prompt
-;; from emacs (especially on Microsoft Windows)
 (prefer-coding-system 'utf-8)
 
-;; Inhibit resizing frame
+(set-charset-priority 'unicode)
+
+(setq system-time-locale "C")
+
 (setq frame-inhibit-implied-resize t)
 
-;; Faster to disable these here (before they've been initialized)
 (push '(menu-bar-lines . 0) default-frame-alist)
+
 (push '(tool-bar-lines . 0) default-frame-alist)
+
 (push '(vertical-scroll-bars) default-frame-alist)
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
+
 (setq-default mode-line-format t)
 
-;;; end.
+(setq gc-cons-threshold most-positive-fixnum)
+
+(setq inhibit-startup-message t)
+
+(add-to-list 'default-frame-alist
+	     '(font . "IBM Plex Mono"))
+
+;;end
