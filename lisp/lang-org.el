9e122786b0be5427bfc501014d65463a31aa26be
Iris Lennon <64725627+ningxilai@users.noreply.github.com>
2025-06-13 12:38:19 +0800

    typo(4)

--- lisp/lang-org.el
+++ lisp/lang-org.el
@@ -4,22 +4,55 @@
 
 (use-package org
   :ensure t
-  :config (setq
+  :config
+  (setq
            ;; Edit settings
            org-auto-align-tags nil
            org-tags-column 0
            org-catch-invisible-edits 'show-and-error
            org-special-ctrl-a/e t
            org-insert-heading-respect-content t
-           
+           org-startup-with-inline-images t
+           org-src-window-setup 'current-window
            ;; Org styling, hide markup etc.
            org-hide-emphasis-markers t
+           org-hide-leading-stars t
            org-pretty-entities t
            org-agenda-tags-column 0
-           org-ellipsis "…"))
+           org-ellipsis "…")
+  (setq
+   org-todo-keywords
+   '((sequence
+      "TODO(t)"
+      "WAIT(w)"
+      "|"                 ; Separates "active" and "inactive" states.
+      "DONE(d)"
+      "CANCELLED(c)" )))
+  :hook ((org-mode . org-indent-mode)
+         (org-mode . prettify-symbols-mode)
+         (org-mode . org-toggle-pretty-entities)
+         (org-mode . (lambda () (visual-line-mode t))))
+  :custom (custom-set-faces
+           '(org-level-1 ((t (:inherit outline-1 :height 1.3))))
+           '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
+           '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
+           '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+           '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+  )
 (use-package org-contrib :ensure t)
+
+(use-package org-appear
+  :ensure t
+  :hook (org-mode . org-appear-mode))
+
+(use-package toc-org
+  :ensure t
+  :after org-mode
+  :hook (org-mode . toc-org-mode))
+
 (use-package htmlize
   :ensure t)
+
 (use-package org-modern
   :ensure t
   :hook ((org-mode . org-modern-mode)
@@ -29,6 +62,12 @@
                               ;; Disable Prettify Symbols mode
                               (setq prettify-symbols-alist nil)
                               (prettify-symbols-mode -1)))))
+
+(use-package org-modern-indent
+    :after org-modern
+    :vc (org-modern-indent :url "https://github.com/jdtsmith/org-modern-indent"
+                           :rev :newest)
+    :hook (org-indent-mode . org-modern-indent-mode))
 
 (provide 'lang-org)
 
